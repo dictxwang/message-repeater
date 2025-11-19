682c8c2ebc3d3619a84e52a841307834a2139f75
dictwang <dictwang@163.com>
2025-11-19 11:48:00 +0800

    add sleep time for subscribe wirte loop

--- src/subscriber/subscriber_acceptor.cpp
+++ src/subscriber/subscriber_acceptor.cpp
@@ -9,7 +9,7 @@ namespace subscriber {
         shared_ptr<bool> connection_alived = std::make_shared<bool>(true);
         thread write_thread([this, client_fd, client_ip, client_port, &config, &context, connection_alived] {
             while (true) {
-                this_thread::sleep_for(chrono::microseconds(1));
+                this_thread::sleep_for(chrono::microseconds(50));
                 if (!(*connection_alived)) {
                     break;
                 }
